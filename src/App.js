db063c05a01e6c8ef2b07aedb1a68a1a53c36fda
hetaljoshi2425 <hetaljoshi2425@gmail.com>
2023-10-24 12:41:38 +0530

    comment code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
